60b66dd6e6e584efbe288dd6b8cdf6858af8a051
AkarshanGhosh <ronighosh494@gmail.com>
2024-07-20 23:42:55 +0530

    admin issue

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,29 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
-
-from login import settings
-#from .views import fetch_data
-#from data1 import fetch_data
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Added this line for admin interface
     path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     path('alert/', views.alert, name='alert'),
     path('google-auth/', views.google_auth, name='google_auth'),
     path('oauth2callback/', views.oauth2callback, name='oauth2callback'),
-    #path('fetch-data/', fetch_data, name='fetch_data'),
-    #path('fetch-data/', fetch_data, name='fetch_data'),
-
+    
     # Password reset URLs
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
 ]
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # type: ignore
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # type: ignore
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
