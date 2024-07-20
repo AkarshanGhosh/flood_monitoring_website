90871ec4189e5494f3db3ca3ede7266f7dd8dd7f
AkarshanGhosh <ronighosh494@gmail.com>
2024-07-20 13:13:25 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,5 @@
 
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-
-echo "Running collectstatic..."
 python3.9 manage.py collectstatic --noinput --clear
-
-echo "Contents of staticfiles_build directory:"
-ls -al staticfiles_build
-
-echo "Contents of staticfiles_build/static directory:"
-ls -al staticfiles_build/static
-
 echo "BUILD END"
