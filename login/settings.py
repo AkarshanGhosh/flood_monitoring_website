11fe55e4d848c4710f4be8f46fc6c654fe4698d3
AkarshanGhosh <ronighosh494@gmail.com>
2024-07-20 13:35:59 +0530

    Update settings.py

--- login/settings.py
+++ login/settings.py
@@ -119,18 +119,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Email backend configuration
 EMAIL_BACKEND = 'authentication.email_backend.OAuth2Backend'
