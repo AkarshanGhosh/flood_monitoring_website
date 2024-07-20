739cbae7da8596303389b27f29ff5819bdd149ec
AkarshanGhosh <ronighosh494@gmail.com>
2024-07-20 13:00:01 +0530

    .

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from login import settings
 #from .views import fetch_data
 #from data1 import fetch_data
 from . import views
@@ -20,4 +23,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
 ]
+#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # type: ignore
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # type: ignore
